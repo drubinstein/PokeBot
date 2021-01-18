91ae33df7e27ec8826abcc57f52a6c2ad334d191
David Rubinstein <drubinstein91@gmail.com>
2021-01-17 23:52:29 -0500

    All memory is now offset by 0xC000

--- util/bridge.lua
+++ util/bridge.lua
@@ -12,162 +12,153 @@ local lastHours = 0
 local frames = 0
 
 local function send(prefix, body)
-	if (client) then
-		local message = prefix
-		if (body) then
-			message = message.." "..body
-		end
-		client:send(message..'\n')
-		return true
-	end
+    if (client) then
+        local message = prefix
+        if (body) then message = message .. " " .. body end
+        client:send(message .. '\n')
+        return true
+    end
 end
 
 local function readln()
-	if (client) then
-		local s, status, partial = client:receive('*l')
-		if status == "closed" then
-			client = nil
-			return nil
-		end
-		if s and s ~= '' then
-			return s
-		end
-	end
+    if (client) then
+        local s, status, partial = client:receive('*l')
+        if status == "closed" then
+            client = nil
+            return nil
+        end
+        if s and s ~= '' then return s end
+    end
 end
 
 -- Wrapper functions
 
 function bridge.init()
-	-- print("Bridge initializing")
-	client = socket.connect("localhost", 16834)
-	-- print("Bridge initialized")
+    -- print("Bridge initializing")
+    client = socket.connect("localhost", 16834)
+    -- print("Bridge initialized")
 end
 
 function bridge.tweet(message) -- Two of the same tweet in a row will only send one
-	print('tweet::'..message)
-	-- return send("tweet", message)
-	return true
+    print('tweet::' .. message)
+    -- return send("tweet", message)
+    return true
 end
 
 function bridge.pollForName()
-	bridge.polling = true
-	-- send("poll_name")
+    bridge.polling = true
+    -- send("poll_name")
 end
 
 function bridge.chat(message, extra)
-	-- print("Bridge Chat")
-	if (extra) then
-		print(message.." || "..extra)
-	else
-		print(message)
-	end
-	-- return send("msg", message)
-	return true
+    -- print("Bridge Chat")
+    if (extra) then
+        print(message .. " || " .. extra)
+    else
+        print(message)
+    end
+    -- return send("msg", message)
+    return true
 end
 
 function bridge.time()
-	if (not timeStopped) then
-		local seconds = memory.raw(0xDA44)
-		local minutes = memory.raw(0xDA43)
-		local hours = memory.raw(0xDA41)
+    if (not timeStopped) then
+        local seconds = memory.raw(0x1A44)
+        local minutes = memory.raw(0x1A43)
+        local hours = memory.raw(0x1A41)
 
-		if (hours ~= lastHours or minutes ~= lastMinutes or seconds ~= lastSeconds) then
-			frames = 0
-			lastSeconds = seconds
-			lastMinutes = minutes
-			lastHours = hours
-		end
-		seconds = seconds + frames / 60
+        if (hours ~= lastHours or minutes ~= lastMinutes or seconds ~=
+            lastSeconds) then
+            frames = 0
+            lastSeconds = seconds
+            lastMinutes = minutes
+            lastHours = hours
+        end
+        seconds = seconds + frames / 60
 
-		if (seconds < 10) then
-			seconds = "0"..seconds
-		end
-		if (minutes < 10) then
-			minutes = "0"..minutes
-		end
-		local message = hours..":"..minutes..":"..seconds
-		send("setgametime", message)
-		frames = frames + 1
-	end
+        if (seconds < 10) then seconds = "0" .. seconds end
+        if (minutes < 10) then minutes = "0" .. minutes end
+        local message = hours .. ":" .. minutes .. ":" .. seconds
+        send("setgametime", message)
+        frames = frames + 1
+    end
 end
 
 function bridge.stats(message)
-	-- print("Bridge Stats")
-	-- return send("stats", message)
-	return true
+    -- print("Bridge Stats")
+    -- return send("stats", message)
+    return true
 end
 
 function bridge.command(command)
-	-- print("Bridge Command")
-	return send(command)
+    -- print("Bridge Command")
+    return send(command)
 end
 
 function bridge.comparisonTime()
-	-- print("Bridge Comparison Time")
-	return send("getcomparisonsplittime")
+    -- print("Bridge Comparison Time")
+    return send("getcomparisonsplittime")
 end
 
 function bridge.process()
-	local response = readln()
-	if (response) then
-		-- print('>'..response)
-		if (response:find("name:")) then
-			return response:gsub("name:", "")
-		else
+    local response = readln()
+    if (response) then
+        -- print('>'..response)
+        if (response:find("name:")) then
+            return response:gsub("name:", "")
+        else
 
-		end
-	end
+        end
+    end
 end
 
 function bridge.input(key)
-	-- send("input", key)
+    -- send("input", key)
 end
 
 function bridge.caught(name)
-	if (name) then
-		-- send("caught", name)
-	end
+    if (name) then
+        -- send("caught", name)
+    end
 end
 
 function bridge.hp(curr, max)
-	-- send("hp", curr..","..max)
+    -- send("hp", curr..","..max)
 end
 
 function bridge.liveSplit()
-	-- print("Bridge Start Timer")
-	send("pausegametime")
-	send("starttimer")
-	timeStopped = false
+    -- print("Bridge Start Timer")
+    send("pausegametime")
+    send("starttimer")
+    timeStopped = false
 end
 
 function bridge.split(encounters, finished)
-	-- print("Bridge Split")
-	if (encounters) then
-		-- database.split(utils.igt(), encounters)
-	end
-	if (finished) then
-		timeStopped = true
-	end
-	send("split")
+    -- print("Bridge Split")
+    if (encounters) then
+        -- database.split(utils.igt(), encounters)
+    end
+    if (finished) then timeStopped = true end
+    send("split")
 end
 
 function bridge.encounter()
-	-- send("encounter")
+    -- send("encounter")
 end
 
 function bridge.reset()
-	-- print("Bridge Reset")
-	send("reset")
-	timeStopped = false
+    -- print("Bridge Reset")
+    send("reset")
+    timeStopped = false
 end
 
 function bridge.close()
-	-- print("Bridge closing")
-	if client then
-		client:close()
-		client = nil
-	end
-	-- print("Bridge closed")
+    -- print("Bridge closing")
+    if client then
+        client:close()
+        client = nil
+    end
+    -- print("Bridge closed")
 end
 
 return bridge
--- util/memory.lua
+++ util/memory.lua
@@ -1,138 +1,135 @@
 local memory = {}
 
+local lunajson = require("lunajson")
+local bridge = require "util.bridge"
+
 local memoryNames = {
-	setting = {
-		text_speed = 0x0D3D,
-		battle_animation = 0x0D3E,
-		battle_style = 0x0D3F,
-		yellow_bitmask = 0x1354,
-	},
-	menu = {
-		settings_row = 0x0C24,
-		column = 0x0C25,
-		row = 0x0C26,
-		current = 0x1FFC,
-		main_current = 0x0C27,
-		input_row = 0x0C2A,
-		size = 0x0C28,
-		pokemon = 0x0C51,
-		shop_current = 0x0C52,
-		transaction_current = 0x0F8B,
-		selection = 0x0C30,
-		selection_mode = 0x0C35,
-		scroll_offset = 0x0C36,
-		text_input = 0x04B6,
-		text_length = 0x0EE9,
-		main = 0x1FF5,
-	},
-	player = {
-		name = 0xD158,
-		name2 = 0xD159,
-		moving = 0x1528,
-		x = 0xD362,
-		y = 0xD361,
-		facing = 0x152A,
-		repel = 0x10DB,
-		party_size = 0xD163,
-	},
-	game = {
-		map = 0xD35E,
-		frames = 0xDA45,
-		battle = 0xD057,
-		textbox = 0x0FC4,
-	},
-	shop = {
-		transaction_amount = 0x0F96,
-	},
-	progress = {
-		trashcans = 0x1773,
-	},
-	pokemon = {
-		exp1 = 0xD179,
-		exp2 = 0xD17A,
-		exp3 = 0xD17B,
-	},
-	battle = {
-		confused = 0x106B,
-		turns = 0x1067,
-		text = 0x1125,
-		menu = 0x0C50,
-		accuracy = 0x0D1E,
-		x_accuracy = 0x1063,
-		disabled = 0x0CEE,
-		paralyzed = 0x1018,
+    setting = {
+        text_speed = 0x0D3D,
+        battle_animation = 0x0D3E,
+        battle_style = 0x0D3F,
+        yellow_bitmask = 0x1354
+    },
+    menu = {
+        settings_row = 0x0C24,
+        column = 0x0C25,
+        row = 0x0C26,
+        current = 0x1FFC,
+        main_current = 0x0C27,
+        input_row = 0x0C2A,
+        size = 0x0C28,
+        pokemon = 0x0C51,
+        shop_current = 0x0C52,
+        transaction_current = 0x0F8B,
+        selection = 0x0C30,
+        selection_mode = 0x0C35,
+        scroll_offset = 0x0C36,
+        text_input = 0x04B6,
+        text_length = 0x0EE9,
+        main = 0x1FF5
+    },
+    player = {
+        name = 0x1158,
+        name2 = 0x1159,
+        moving = 0x1528,
+        x = 0x1362,
+        y = 0x1361,
+        facing = 0x152A,
+        repel = 0x10DB,
+        party_size = 0x1163
+    },
+    game = {map = 0x135E, frames = 0x1A45, battle = 0x1057, textbox = 0x0FC4},
+    shop = {transaction_amount = 0x0F96},
+    progress = {trashcans = 0x1773},
+    pokemon = {exp1 = 0x1179, exp2 = 0x117A, exp3 = 0x117B},
+    battle = {
+        confused = 0x106B,
+        turns = 0x1067,
+        text = 0x1125,
+        menu = 0x0C50,
+        accuracy = 0x0D1E,
+        x_accuracy = 0x1063,
+        disabled = 0x0CEE,
+        paralyzed = 0x1018,
 
-		opponent_move = 0x0FEE,
-		critical = 0x105E,
+        opponent_move = 0x0FEE,
+        critical = 0x105E,
 
-		opponent_bide = 0x106F,
-		opponent_id = 0xCFE5,
-		opponent_level = 0xCFF3,
-		opponent_type1 = 0xCFEA,
-		opponent_type2 = 0xCFEB,
+        opponent_bide = 0x106F,
+        opponent_id = 0x0FE5,
+        opponent_level = 0x0FF3,
+        opponent_type1 = 0x0FEA,
+        opponent_type2 = 0x0FEB,
 
-		our_id = 0xD014,
-		our_status = 0xD018,
-		our_level = 0xD022,
-		our_type1 = 0xD019,
-		our_type2 = 0xD01A,
-	},
+        our_id = 0x1014,
+        our_status = 0x1018,
+        our_level = 0x1022,
+        our_type1 = 0x1019,
+        our_type2 = 0x101A
+    }
 }
 
 local doubleNames = {
-	pokemon = {
-		attack = 0xD17E,
-		defense = 0xD181,
-		speed = 0xD183,
-		special = 0xD185,
-	},
-	battle = {
-		opponent_hp = 0xCFE6,
-		opponent_max_hp = 0xCFF4,
-		opponent_attack = 0xCFF6,
-		opponent_defense = 0xCFF8,
-		opponent_speed = 0xCFFA,
-		opponent_special = 0xCFFC,
+    pokemon = {
+        attack = 0x117E,
+        defense = 0x1181,
+        speed = 0x1183,
+        special = 0x1185
+    },
+    battle = {
+        opponent_hp = 0x0FE6,
+        opponent_max_hp = 0x0FF4,
+        opponent_attack = 0x0FF6,
+        opponent_defense = 0x0FF8,
+        opponent_speed = 0x0FFA,
+        opponent_special = 0x0FFC,
 
-		our_hp = 0xD015,
-		our_max_hp = 0xD023,
-		our_attack = 0xD025,
-		our_defense = 0xD027,
-		our_speed = 0xD029,
-		our_special = 0xD02B,
-	},
+        our_hp = 0x1015,
+        our_max_hp = 0x1023,
+        our_attack = 0x1025,
+        our_defense = 0x1027,
+        our_speed = 0x1029,
+        our_special = 0x102B
+    }
 }
 
+--[[
 local function raw(value)
 	return mainmemory.readbyte(value)
+end
+--]]
+local function raw(value)
+    local json_msg = lunajson.encode({
+        ["method"] = "memory",
+        ["payload"] = value + 0x0000 -- the RAM space
+    })
+    local mem_resp = bridge.send(json_msg)
+    return mem_resp["memory"]
 end
 memory.raw = raw
 
 function memory.string(first, last)
-	local a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ():;[]abcdefghijklmnopqrstuvwxyz?????????????????????????????????????????-???!.????????*?/.?0123456789"
-	local str = ""
-	while first <= last do
-		local v = raw(first) - 127
-		if v < 1 then
-			return str
-		end
-		str = str..string.sub(a, v, v)
-		first = first + 1
-	end
-	return str
+    local a =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ():;[]abcdefghijklmnopqrstuvwxyz?????????????????????????????????????????-???!.????????*?/.?0123456789"
+    local str = ""
+    while first <= last do
+        local v = raw(first) - 127
+        if v < 1 then return str end
+        str = str .. string.sub(a, v, v)
+        first = first + 1
+    end
+    return str
 end
 
 function memory.double(section, key)
-	local first = doubleNames[section][key]
-	return raw(first) + raw(first + 1)
+    local first = doubleNames[section][key]
+    return raw(first) + raw(first + 1)
 end
 
 function memory.value(section, key)
-	local memoryAddress = memoryNames[section]
-	if (key) then
-		memoryAddress = memoryAddress[key]
-	end
-	return raw(memoryAddress)
+    local memoryAddress = memoryNames[section]
+    if (key) then memoryAddress = memoryAddress[key] end
+    return raw(memoryAddress)
 end
 
 return memory
