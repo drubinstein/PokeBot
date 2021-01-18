4865e4999b8c7f8bdddba1f85bd35fece2a49377
David Rubinstein <drubinstein91@gmail.com>
2021-01-18 00:06:18 -0500

    Input is now through bridge

--- util/input.lua
+++ util/input.lua
@@ -3,22 +3,25 @@ local input = {}
 local bridge = require "util.bridge"
 local memory = require "util.memory"
 
+local lunajson = require "lunajson"
+
 local lastSend
 local currentButton, remainingFrames, setForFrame
-local debug
+local _debug
 local bCancel = true
 
 local function bridgeButton(btn)
-	if (btn ~= lastSend) then
-		lastSend = btn
-		bridge.input(btn)
-	end
+    if (btn ~= lastSend) then
+        lastSend = btn
+        -- bridge.input(btn)
+    end
 end
 
+--[[
 local function sendButton(button, ab)
 	local inputTable = {[button] = true}
 	joypad.set(inputTable)
-	if (debug) then
+	if (_debug) then
 		gui.text(0, 7, button.." "..remainingFrames)
 	end
 	if (ab) then
@@ -27,93 +30,96 @@ local function sendButton(button, ab)
 	bridgeButton(button)
 	setForFrame = button
 end
+--]]
+
+local function sendButton(button, ab)
+    -- print(debug.traceback())
+    -- local inputTable = {[button] = true}
+    bridge.send(lunajson.encode({["method"] = "button", ["payload"] = button}))
+    if (_debug) then print(button .. " " .. remainingFrames) end
+    if (ab) then button = "AB" end
+    setForFrame = button
+end
 
 function input.press(button, frames)
-	if (setForFrame) then
-		print("ERR: Reassigning "..setForFrame.." to "..button)
-		return
-	end
-	if (frames == nil or frames > 0) then
-		if (button == currentButton) then
-			return
-		end
-		if (not frames) then
-			frames = 1
-		end
-		currentButton = button
-		remainingFrames = frames
-	else
-		remainingFrames = 0
-	end
-	bCancel = button ~= "B"
-	sendButton(button)
+    if (setForFrame) then
+        print("ERR: Reassigning " .. setForFrame .. " to " .. button)
+        return
+    end
+    if (frames == nil or frames > 0) then
+        if (button == currentButton) then return end
+        if (not frames) then frames = 1 end
+        currentButton = button
+        remainingFrames = frames
+    else
+        remainingFrames = 0
+    end
+    bCancel = button ~= "B"
+    sendButton(button)
 end
 
 function input.cancel(accept)
-	if (accept and memory.value("menu", "shop_current") == 20) then
-		input.press(accept)
-	else
-		local button
-		if (bCancel) then
-			button = "B"
-		else
-			button = "A"
-		end
-		remainingFrames = 0
-		sendButton(button, true)
-		bCancel = not bCancel
-	end
+    if (accept and memory.value("menu", "shop_current") == 20) then
+        input.press(accept)
+    else
+        local button
+        if (bCancel) then
+            button = "B"
+        else
+            button = "A"
+        end
+        remainingFrames = 0
+        sendButton(button, true)
+        bCancel = not bCancel
+    end
 end
 
 function input.escape()
-	local inputTable = {Right=true, Down=true}
-	joypad.set(inputTable)
-	bridgeButton("Escape")
+    local inputTable = {Right = true, Down = true}
+    -- joypad.set(inputTable)
+    bridge.send(lunajson.encode({["method"] = "button", ["payload"] = "right"}))
+    bridge.send(lunajson.encode({["method"] = "button", ["payload"] = "down"}))
+    bridgeButton("Escape")
 end
 
 function input.clear()
-	currentButton = nil
-	remainingFrames = -1
+    currentButton = nil
+    remainingFrames = -1
 end
 
 function input.update()
-	if (currentButton) then
-		remainingFrames = remainingFrames - 1
-		if (remainingFrames >= 0) then
-			if (remainingFrames > 0) then
-				sendButton(currentButton)
-				return true
-			end
-		else
-			currentButton = nil
-		end
-	end
-	setForFrame = nil
+    if (currentButton) then
+        remainingFrames = remainingFrames - 1
+        if (remainingFrames >= 0) then
+            if (remainingFrames > 0) then
+                sendButton(currentButton)
+                return true
+            end
+        else
+            currentButton = nil
+        end
+    end
+    setForFrame = nil
 end
 
 function input.advance()
-	if (not setForFrame) then
-		bridgeButton("e")
-		-- print("e")
-	end
+    if (not setForFrame) then
+        bridgeButton("e")
+        -- print("e")
+    end
 end
 
-function input.setDebug(enabled)
-	debug = enabled
-end
+function input.setDebug(enabled) _debug = enabled end
 
 function input.test(fn, completes)
-	while (true) do
-		if (not input.update()) then
-			if (fn() and completes) then
-				break
-			end
-		end
-		emu.frameadvance()
-	end
-	if (completes) then
-		print(completes.." complete!")
-	end
+    while (true) do
+        if (not input.update()) then
+            if (fn() and completes) then break end
+        end
+        -- emu.frameadvance()
+        bridge.send(lunajson.encode({["method"] = "frame_advance"}))
+    end
+    if (completes) then print(completes .. " complete!") end
 end
 
 return input
